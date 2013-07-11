b2cbb60665c3e3df91a4d4cb57dda478757e6feb
d0ge01 <not.d0ge@anche.no>
2013-07-11 11:31:59 +0200

    added debug info into main program

--- system/cpmf.rb
+++ system/cpmf.rb
@@ -5,15 +5,13 @@ require 'socket'
 # Salvatore Criscione <salvatore@grrlz.net>
 
 class CaptivePortal
-  attr_accessor :port, :iptables_bin, :allowedDB, :deniedDB, :interface, :network_lan, :active, :nIpAllowed, :nIpBanned, :server, :db, :debug
+  attr_accessor :port, :iptables_bin, :allowedDB, :deniedDB, :interface, :network_lan, :active, :server, :db, :debug
   def initialize(port, debug)
     self.allowedDB = []  # Array that contains all ip allowed
     self.deniedDB = ["192.168.1.1","127.0.0.1","10.10.10.10"]   # Array that contains all banned ip
 	self.blockConnection
 	self.debug = debug == "true" ? true : false
     self.port = port.to_i
-    self.nIpAllowed = 0
-    self.nIpBanned = 0
 	self.active = false;
 	self.iptables_bin = "/sbin/ipables"
 	
@@ -27,19 +25,39 @@ class CaptivePortal
 	# Always last line of initialize.
 	self.listenAsk
   end
+  
   def loadDB
-	self.db = SQLite3::Database.new "login.db"
+	begin
+		self.db = SQLite3::Database.new "login.db"
+		puts "[!] Inizialized database" if self.debug
+	rescue
+		self.error('database error')
+	end
   end
+  
   def createTable
-	self.db.execute "CREATE TABLE IF NOT EXISTS logindata (email TEXT, password TEXT, Id INTEGER PRIMARY KEY AUTOINCREMENT)"
+	begin
+		self.db.execute "CREATE TABLE IF NOT EXISTS logindata (email TEXT, password TEXT, Id INTEGER PRIMARY KEY AUTOINCREMENT)"
+		puts "[!] Created table if not exists" if self.debug
+	rescue
+		self.error('create table error')
+	end
   end
+  
   def addUser(email, password)
-    self.db.execute "INSERT INTO logindata VALUES('#{email}','#{password}',null)"
+	begin
+		self.db.execute "INSERT INTO logindata VALUES('#{email}','#{password}',null)"
+		puts "[!] Added User #{email},#{password} to database" if self.debug
+	rescue
+		self.error('add user error')
+	end
   end
+  
   def blockConnection
 	system("#{self.iptables_bin} -A PREROUTING -i #{self.interface} -m tcp -s #{self.network_lan} --dport 80 -j REDIRECT --to-ports 3128 ");
 	# Apache with configuration running on ports 3128
   end
+  
   def addNewIpAllowed(ip)
     self.allowedDB << ip
     self.nIpAllowed += 1
@@ -55,8 +73,9 @@ class CaptivePortal
       self.deban(ip)
     end
   end
+  
   def checkLogin(email, password)
-	puts "Login checker activated: email = #{email} , password = #{password}" if self.debug
+	puts "[!] Login checker activated: email = #{email} , password = #{password}" if self.debug
 	rs = self.db.execute"SELECT * FROM logindata WHERE email='#{email}' AND password='#{password}'"
 	puts "Returned #{rs.size} rows.." if self.debug
 	if rs.size == 0
@@ -65,6 +84,7 @@ class CaptivePortal
 		return true
 	end
   end
+  
   def status
 	buff = ""
 	if self.active
@@ -73,38 +93,50 @@ class CaptivePortal
 		buff = "<font color='red'>OFFLINE</font>"
 	end
 	"Status: #{buff} </br>\n " + 
-	"Ip Banned: #{self.nIpBanned}</br>\n" + 
-	"Ip Allowed:#{self.nIpAllowed}</br>\n" + 
+	"Ip Allowed: #{self.allowedDB.size}</br>\n" + 
+	"Ip Banned:#{self.deniedDB.size}</br>\n" + 
 	"Server running in the port: #{self.port}</br>\n"
   end
+  
   def listenAsk
-	self.server = TCPServer.open(self.port)
-	puts "Server started on port: #{self.port}"
+	begin
+		self.server = TCPServer.open(self.port)
+	rescue
+		self.error('server start error')
+	end
+	puts "[+] Server started on port: #{self.port}"
 	self.active = true;
 	loop {
 	  Thread.start(server.accept) do |client|
 		data = client.recv(1024)
-		puts "Data received: #{data}"
+		puts "[!] Data received: #{data}" if self.debug
 		data = data.split(' ')
 		if data.first == "status"
+			puts "[!] sending status to client" if self.debug
 			client.puts self.status
 		end
+		
 		if data.first == "login"
 			if(self.checkLogin(data[1],data[2]))
+				puts "[!] Login OK" if self.debug
 				client.puts "OK"
 			else
+				puts "[!] Login FAIL" if self.debug
 				client.puts "FAIL"
 			end
 			client.puts "END"
 		end
+		
 		if data.first == "ipbanned"
 			client.puts self.deniedDB.join(' </br>- ')
 		end
+		
 		if data.first == "exit"
 			if data[1] == "helloworld"
 				exit
 			end
 		end
+		
 		if data.first == "register"
 			self.addUser(data[1],data[2])
 		end
@@ -112,6 +144,7 @@ class CaptivePortal
 		if data.first == "deactivate"
 			self.active = false
 		end
+		
 		if data.first == "activate"
 			self.active == true
 		end
@@ -119,12 +152,19 @@ class CaptivePortal
 	  end
 	}
   end
+  
   def deban(ip)
     sleep 60*60 # 1 hours
     self.deniedDB = self.deniedDB - [ ip ]
     self.nIpBanned -= 1
   end
+  
+  def error(txt)
+	puts "[-] Error: #{txt}"
+	exit
+  end
 end
+
 if ARGV.size >= 2
 	cp = CaptivePortal.new(ARGV.shift, ARGV.shift)
 end
